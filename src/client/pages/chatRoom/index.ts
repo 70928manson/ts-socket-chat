9ff115e21f606da54ee061ef0bdb464a7a08d0f9
Manson <manson972@yahoo.com>
2023-06-10 15:33:53 +0800

    input userName and roomName update

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -1,9 +1,17 @@
 import "./index.css";
 import { io } from 'socket.io-client';
 
-import { name } from "@/utils";
+const url = new URL(location.href);
+const userName = url.searchParams.get('user_name');
+const roomName = url.searchParams.get('room_name');
 
-console.log("client side chatroom page", name);
+console.log('chat', userName, roomName);
+
+//避免使用者直接進到聊天室畫面，確保輸入userName和選擇房間才能進去
+if (!userName || !roomName) {
+    location.href = '/main/main.html';
+}
+
 
 //1. 建立連接 -> node server
 const clientIo = io();
--- src/client/pages/main/index.ts
+++ src/client/pages/main/index.ts
@@ -1,4 +1,13 @@
 import "./index.css";
-import { name } from "@/utils";
 
-console.log("client side main page", name);
+//下斷點 as 判斷
+const nameInput = document.getElementById("nameInput") as HTMLInputElement;
+const roomSelect = document.getElementById("roomSelect") as HTMLSelectElement;
+const startBtn = document.getElementById("startBtn") as HTMLButtonElement;
+
+startBtn.addEventListener('click', () => {
+    const userName = nameInput.value;
+    const roomName = roomSelect.value;
+
+    location.href = `/chatRoom/chatRoom.html?user_name=${userName}&room_name=${roomName}`
+})
